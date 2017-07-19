e965f29e5a2c6700955c216b7d01ab05190ad46c
TheLostCookie <TheLostCookie@3454003.no-reply.drupal.org>
2017-07-19 10:43:55 -0500

    update to javascript library

--- web/themes/main/assets/js/Library.js
+++ web/themes/main/assets/js/Library.js
@@ -1,3 +1,162 @@
-export function cube(x) {
-  return x * x * x;
-}
+/**
+ *
+ * @param element
+ * @param className
+ */
+export const eClick = (element, className) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(!element.classList.contains(className)) {
+      element.classList.add(className);
+    } else {
+      element.classList.remove(className);
+    }
+  })
+};
+
+/**
+ *
+ * @param elements
+ * @param className
+ */
+export const eMultiClick = (elements, className) => {
+  for(let i = 0; i < elements.length; i++) {
+    eClick(elements[i], className);
+  }
+};
+
+/**
+ *
+ * @param element
+ * @param menuElement
+ * @param animation
+ */
+export const eMobileMenu = (settings) => {
+  const OPEN = 'open';
+  const FADE = settings.direction === 'left' ? 'navFadeInLeft' : 'navFadeInRight';
+  const CLOSE = settings.direction === 'left' ? 'navFadeOutLeft' : 'navFadeOutRight';
+
+  settings.element.addEventListener('click', () => {
+    if(!settings.element.classList.contains(OPEN)) {
+      settings.element.classList.add(OPEN);
+      settings.menu.classList.remove(CLOSE);
+      settings.menu.classList.add(OPEN);
+      settings.menu.classList.add(FADE);
+    } else {
+      settings.element.classList.remove(OPEN);
+      settings.menu.classList.remove(FADE);
+      settings.menu.classList.remove(OPEN);
+      settings.menu.classList.add(CLOSE);
+    }
+  })
+};
+
+export const eMenu = (element) => {
+  const LINKS = element.querySelectorAll('li');
+  for (let i = 0; i < LINKS.length; i++) {
+    if(LINKS[i].classList.contains('menu-item--expanded')) {
+      LINKS[i].addEventListener('click', (e) => {
+        if(LINKS[i].classList.contains('hover') && !e.target.parentElement.classList.contains('menu-item--expanded')) {
+          LINKS[i].classList.remove('hover');
+          element.classList.remove('hover-open');
+        } else if(e.target.parentElement.classList.contains('hover')) {
+          e.preventDefault();
+          LINKS[i].classList.remove('hover');
+          element.classList.remove('hover-open');
+        } else {
+          e.preventDefault();
+          e.stopPropagation();
+          LINKS[i].classList.add('hover');
+          element.classList.add('hover-open')
+        }
+      });
+    }
+  }
+
+  window.addEventListener('click', () => {
+    if(element.classList.contains('hover-open')) {
+      element.classList.remove('hover-open');
+      for (let i = 0; i < LINKS.length; i++ ) {
+        if(LINKS[i].classList.contains('menu-item--expanded')) {
+          LINKS[i].classList.remove('hover');
+        }
+      }
+    }
+  });
+};
+
+/**
+ *
+ * @param element
+ * @param type
+ * @param posFromTop
+ */
+export const animate = (element, type, posFromTop) => {
+  let windowHeight = window.innerHeight;
+  let elementPosFromTop = element.getBoundingClientRect().top;
+
+
+  if((elementPosFromTop + posFromTop) <= windowHeight && !element.classList.contains(type)) {
+    element.classList.add(type);
+  }
+};
+
+/**
+ *
+ * @param func
+ * @param wait
+ * @param immediate
+ * @returns {function()}
+ */
+export const debounce = (func, wait, immediate) => {
+  let timeout;
+  return () => {
+    let context = this, args = arguments;
+    let later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+export const toTop = (element, speed) => {
+  element.addEventListener('click', () => {
+    //set initial speed of the scroll
+    let scrollStep = -window.scrollY / (300 / speed),
+      // send it into an interval function
+      scrollInterval = setInterval(() => {
+        if ( window.scrollY !== 0 ) {
+          window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval);
+      },speed);
+  });
+};
+
+/**
+ *
+ * @returns {*}
+ */
+export const screenCategory = () => {
+  let width = window.innerWidth;
+  let size;
+
+  const screenSizes = {
+    mobile: {title: 'mobile', width: 320},
+    tablet: {title: 'tablet', width: 680},
+    desktop: {title: 'desktop', width: 1000},
+  };
+
+  if(width < screenSizes.tablet.width) {
+    size = screenSizes.mobile.title;
+  } else if(width >= screenSizes.tablet.width && width < screenSizes.desktop.width) {
+    size = screenSizes.tablet.title;
+  } else {
+    size = screenSizes.desktop.title;
+  }
+  return size;
+};
--- web/themes/main/assets/js/scripts.js
+++ web/themes/main/assets/js/scripts.js
@@ -1,14 +1,66 @@
-import {cube} from './Library'
-
+import {
+  eClick,
+  eMultiClick,
+  eMobileMenu,
+  eMenu,
+  animate,
+  debounce,
+  screenCategory,
+  toTop}
+  from './Library';
 
 (function (Drupal, $) {
-  console.log(cube(10));
-  console.log('hello jeff');
-  //inherit drupal behaviors
+  //Global Project Variables and elements
+  const MAIN_MENU = document.querySelector('#block-main-main-menu .menu'); //main menu
+  const Burger_Button = document.getElementById('burger-icon'); // burger icon
+  //This object is used store settings for mobile menu
+  const MOBILE_MENU_SETTINGS = {
+    element: Burger_Button,
+    menu: MAIN_MENU,
+    direction: 'right', //options are left and right
+  };
+  //The variance in when the function on the scroll listener will fire
+  const SCROLL_DEBOUNCE = 5;
+  //The variance in when the function on the resize listener will fire
+  const RESIZE_DEBOUNCE = 5;
+
+  //Use this to interact with drupal behaviors module
   Drupal.behaviors.main = {
     attach: function (context, settings) {
     }//end of inherited context and settings from drupal
   }//end of drupal behaviors
+  /* global Javascript */
+
+
+  //Create mobile menu
+  eMobileMenu(MOBILE_MENU_SETTINGS);
+
+  //create main navigation menu
+  eMenu(MAIN_MENU);
+
+  //on scroll event listener comment out if you don't need it.
+  window.addEventListener('scroll', debounce(() => {
+    //example of calling animation function
+    //animate(element,null, 100);
+    }
+  , SCROLL_DEBOUNCE));
+
+
+  window.addEventListener('resize', debounce(() => {
+    "use strict";
+    //check if window is at 1000 pixels or larger
+    if(screenCategory() === 'desktop') {
+      //check if mobile menu is still open
+      if(MAIN_MENU.classList.contains('open')) {
+        //check the direction of the animation
+        let close = MOBILE_MENU_SETTINGS.direction === 'left' ? 'navFadeOutLeft' : 'navFadeOutRight';
+        //create click event to close mobile menu
+        MOBILE_MENU_SETTINGS.element.click();
+        //remove the closing animation
+        MOBILE_MENU_SETTINGS.menu.classList.remove(close);
+      }
+    }
+  },RESIZE_DEBOUNCE))
 
 }(Drupal, jQuery)); //end of drupal and jQuery closure
 
