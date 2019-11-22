487fc10ae944e9b9ba404d3d2d1548648de6dce6
Garrett W <garrett@encoremultimedia.com>
2019-11-22 11:10:32 -0600

    updated lando/composer/package stuff

--- web/themes/main/.eslintrc.js
+++ web/themes/main/.eslintrc.js
@@ -10,12 +10,26 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'comma-dangle': ['error', 'always-multiline'],
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'no-unused-vars': ['warn'],
     'no-console': 0,
+    // eslint 5 compat
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "ignore"
+    }],
+    "no-async-promise-executor": "off",
+    "no-misleading-character-class": "off",
+    "no-prototype-builtins": "off",
+    "no-shadow-restricted-names": "off",
+    "no-useless-catch": "off",
+    "no-with": "off",
+    "require-atomic-updates": "off",
   },
 };
